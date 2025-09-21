cf759cd4334e9efa47dbeefb8b0969d84e0931c9
yks-dataterrain <yogesh@dataterrain.com>
2025-09-21 12:12:19 +0530

    yes

---
+++ regex_learning.py
@@ -0,0 +1,15 @@
+'''
+Regular expressions are
+sequences of characters
+that define search patterns.
+'''
+
+import regex as re
+name ="paul walker"
+x = re.search("paul walker",name)
+print(x.group())
+
+
+"""
+https://medium.com/@yogeshkrishnanseeniraj/the-complete-guide-to-regular-expressions-in-python-from-beginner-to-expert-6fd239351f37
+"""
