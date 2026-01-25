4beae6b5d2cbee6152f5ec953e8b92bc2924ae17
yks-dataterrain <yogesh@dataterrain.com>
2026-01-25 16:35:15 +0530

    singleton-class

---
+++ common_design_patterns/singleton.py
@@ -0,0 +1,50 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
+a = Singleton()
+b = Singleton()
+
+print( a is b)
+
+
+#Thread safe Singleton
+import threading 
+
+class ThreadSafeSingleton:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
+        return cls._instance
+
+a = ThreadSafeSingleton()
+b = ThreadSafeSingleton()
+
+print(a is b)
+
+# Metaclass
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Config(metaclass=SingletonMeta):
+    pass
+
+a = Config()
+b = Config()
+
+print(a is b)
+
